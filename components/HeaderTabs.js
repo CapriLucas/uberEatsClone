b97fec5071eaac88269bbe0032039ae13304ce71
Lucas Capriata <lucas.capriata@123seguro.com>
2021-09-23 07:37:12 -0300

    feat: add headerTab component

---
+++ components/HeaderTabs.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+export default function HeaderTabs() {
+  const [activeTab, setActiveTab] = useState('Delivery')
+  return (
+    <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+      <HeaderButton
+        text="Delivery"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        text="Pickup"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+    </View>
+  )
+}
+
+const HeaderButton = ({ text, activeTab, setActiveTab }) => (
+  <TouchableOpacity
+    style={{
+      backgroundColor: activeTab === text ? 'black' : 'white',
+      paddingVertical: 6,
+      paddingHorizontal: 16,
+      borderRadius: 30,
+    }}
+    onPress={() => setActiveTab(text)}
+  >
+    <Text
+      style={{
+        color: activeTab === text ? 'white' : 'black',
+        fontSize: 15,
+        fontWeight: '700',
+      }}
+    >
+      {text}
+    </Text>
+  </TouchableOpacity>
+)
---
+++ screens/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SafeAreaView, StatusBar, View } from 'react-native'
+import HeaderTabs from '../components/HeaderTabs'
+
+export default function Home() {
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: '#eee',
+        flex: 1,
+        marginTop: StatusBar.currentHeight,
+      }}
+    >
+      <View style={{ backgroundColor: 'white', padding: 15 }}>
+        <HeaderTabs />
+      </View>
+    </SafeAreaView>
+  )
+}
