9bc07eb27be6b8aaee6bde49baca29544fbf2741
CapriLucas <capriatalucas@gmail.com>
2021-09-27 17:54:45 -0300

    feat: add restaurantItems component

---
+++ components/RestaurantItems.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+export default function RestaurantItems({ restaurants }) {
+  return (
+    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
+      {restaurants.map((restaurant, index) => (
+        <View
+          key={index}
+          style={{ marginTop: 10, padding: 15, backgroundColor: '#fff' }}
+        >
+          <RestaurantImage image_url={restaurant.image_url} />
+          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      ))}
+    </TouchableOpacity>
+  )
+}
+
+const RestaurantImage = ({ image_url }) => (
+  <>
+    <Image source={{ uri: image_url }} style={{ width: '100%', height: 150 }} />
+    <TouchableOpacity style={{ position: 'absolute', right: 15, top: 15 }}>
+      <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
+    </TouchableOpacity>
+  </>
+)
+
+const RestaurantInfo = ({ name, rating }) => (
+  <View
+    style={{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{name}</Text>
+      <Text style={{ fontSize: 13, color: 'gray' }}>30-45 · min</Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: '#eee',
+        padding: 5,
+        alignItems: 'center',
+        borderRadius: 15,
+      }}
+    >
+      <Text>{rating}</Text>
+    </View>
+  </View>
+)
