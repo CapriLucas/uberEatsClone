828974ec74ebeb24da52c96f6efd2fbf4a55050b
CapriLucas <capriatalucas@gmail.com>
2021-09-27 17:55:45 -0300

    feat: add searchbar functionality

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
-import { SafeAreaView, StatusBar, View } from 'react-native'
+import Axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView, ScrollView, StatusBar, View } from 'react-native'
+import Categories from '../components/Categories'
 import HeaderTabs from '../components/HeaderTabs'
+import RestaurantItems from '../components/RestaurantItems'
+import SearchBar from '../components/SearchBar'
 
+const YELP_API_KEY =
 export default function Home() {
+  const [restaurantsData, setRestaurantsData] = useState([])
+  const [city, setCity] = useState('San Francisco')
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`
+
+    return Axios.get(yelpUrl, {
+      headers: { Authorization: `Bearer ${YELP_API_KEY}` },
+    }).then((res) => setRestaurantsData(res.data.businesses))
+  }
+
+  useEffect(() => {
+    getRestaurantsFromYelp().catch((err) => console.log('error'))
+  }, [city])
+
   return (
     <SafeAreaView
       style={{
@@ -13,7 +32,7 @@ export default function Home() {
     >
       <View style={{ backgroundColor: 'white', padding: 15 }}>
         <HeaderTabs />
-        <SearchBar />
+        <SearchBar cityHandler={setCity} />
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
