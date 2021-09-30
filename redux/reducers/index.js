d540686bc4b19d45b13c7b7b7a3e49372bd8108f
CapriLucas <capriatalucas@gmail.com>
2021-09-30 12:37:55 -0300

    feat: add redux boilerplate

---
+++ redux/reducers/index.js
@@ -0,0 +1,10 @@
+import cartReducer from './cartReducer'
+import { combineReducers } from 'redux'
+
+let reducers = combineReducers({ cartReducer: cartReducer })
+
+const rootReducer = (state, action) => {
+  return reducers(state, action)
+}
+
+export default rootReducer
