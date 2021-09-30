2c6367aae49596ee52a00cce457ebb3523aaf38d
CapriLucas <capriatalucas@gmail.com>
2021-09-30 11:51:47 -0300

    feat: add ViewCart component

--- screens/RestaurantDetails.js
+++ screens/RestaurantDetails.js
@@ -3,13 +3,15 @@ import { View, Text } from 'react-native'
 import { Divider } from 'react-native-elements'
 import About from '../components/restaurantDetails/About'
 import MenuItems from '../components/restaurantDetails/MenuItems'
+import ViewCart from '../components/restaurantDetails/ViewCart'
 
-export default function RestaurantDetails({ route }) {
+export default function RestaurantDetails({ route, navigation }) {
   return (
     <View style={{ flex: 1 }}>
       <About route={route} />
       <Divider width={1.8} style={{ marginTop: 20 }} />
       <MenuItems />
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
     </View>
   )
 }
