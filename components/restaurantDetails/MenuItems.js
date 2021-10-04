b55e3c09018fe74689c59a589f7222f573e7072c
CapriLucas <capriatalucas@gmail.com>
2021-10-04 07:07:19 -0300

    refactor: send foods via props to MenuItems

--- components/restaurantDetails/MenuItems.js
+++ components/restaurantDetails/MenuItems.js
@@ -4,37 +4,6 @@ import { Divider } from 'react-native-elements'
 import BouncyCheckBox from 'react-native-bouncy-checkbox'
 import { useDispatch, useSelector } from 'react-redux'
 
-const foods = [
-  {
-    title: 'Lasagna',
-    description: 'With butter lettuce, tomato and sauce bechamel',
-    price: '$14.51',
-    image:
-      'https://d1uz88p17r663j.cloudfront.net/original/4a783abdbfe1f7a79fbf5f93139b3c68_Lasagna-de-carne-.jpg',
-  },
-  {
-    title: 'Tandoori Chicken',
-    description: 'Amazing Indian dish with tenderloin chicken off the sizzles',
-    price: '$19.51',
-    image:
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chicken-tandori-1526595014.jpg',
-  },
-  {
-    title: 'Chilaquiles',
-    description: "Awesome mexican dish. It'll blow your mind",
-    price: '$14.51',
-    image:
-      'https://estaticos.miarevista.es/uploads/images/recipe/58eba85e5bafe83d203c986b/ppal-chilaquiles_0.jpg',
-  },
-  {
-    title: 'Gnocchi',
-    description: 'With butter',
-    price: '$20.12',
-    image:
-      'https://www.kikkoman.eu/fileadmin/_processed_/a/e/csm_WEB_Spicy_tomato_and_sausage_gnocchi_with_fennel_and_spinach_b1e3a765d7.jpg',
-  },
-]
-
 const styles = StyleSheet.create({
   menuItemStyle: {
     flexDirection: 'row',
@@ -50,7 +19,12 @@ const styles = StyleSheet.create({
   },
 })
 
-export default function MenuItems({ restaurantName }) {
+export default function MenuItems({
+  restaurantName,
+  foods,
+  hideCheckbox,
+  marginLeft,
+}) {
   const dispatch = useDispatch()
   const selectItem = (item) =>
     dispatch({ type: 'ADD_TO_CART', payload: { item, restaurantName } })
@@ -70,21 +44,23 @@ export default function MenuItems({ restaurantName }) {
       {foods.map((food, index) => (
         <View key={index}>
           <View key={index} style={styles.menuItemStyle}>
-            <BouncyCheckBox
-              isChecked={isFoodInCart(food)}
-              iconStyle={{
-                borderColor: 'lightgray',
-                borderRadius: 0,
-              }}
-              fillColor="green"
-              onPress={(isChecked) =>
-                isChecked
-                  ? selectItem({ ...food, isChecked })
-                  : removeItem(food.title)
-              }
-            />
+            {!hideCheckbox && (
+              <BouncyCheckBox
+                isChecked={isFoodInCart(food)}
+                iconStyle={{
+                  borderColor: 'lightgray',
+                  borderRadius: 0,
+                }}
+                fillColor="green"
+                onPress={(isChecked) =>
+                  isChecked
+                    ? selectItem({ ...food, isChecked })
+                    : removeItem(food.title)
+                }
+              />
+            )}
             <FoodInfo food={food} />
-            <FoodImage food={food} />
+            <FoodImage food={food} marginLeft={marginLeft || 0} />
           </View>
           <Divider width={0.4} style={{ marginHorizontal: 20 }} />
         </View>
@@ -101,11 +77,11 @@ const FoodInfo = ({ food }) => (
   </View>
 )
 
-const FoodImage = ({ food }) => (
+const FoodImage = ({ food, marginLeft }) => (
   <View>
     <Image
       source={{ uri: food.image }}
-      style={{ width: 90, height: 90, borderRadius: 8 }}
+      style={{ width: 90, height: 90, borderRadius: 8, marginLeft }}
     />
   </View>
 )
--- screens/RestaurantDetails.js
+++ screens/RestaurantDetails.js
@@ -5,13 +5,44 @@ import About from '../components/restaurantDetails/About'
 import MenuItems from '../components/restaurantDetails/MenuItems'
 import ViewCart from '../components/restaurantDetails/ViewCart'
 
+const foods = [
+  {
+    title: 'Lasagna',
+    description: 'With butter lettuce, tomato and sauce bechamel',
+    price: '$14.51',
+    image:
+      'https://d1uz88p17r663j.cloudfront.net/original/4a783abdbfe1f7a79fbf5f93139b3c68_Lasagna-de-carne-.jpg',
+  },
+  {
+    title: 'Tandoori Chicken',
+    description: 'Amazing Indian dish with tenderloin chicken off the sizzles',
+    price: '$19.51',
+    image:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chicken-tandori-1526595014.jpg',
+  },
+  {
+    title: 'Chilaquiles',
+    description: "Awesome mexican dish. It'll blow your mind",
+    price: '$14.51',
+    image:
+      'https://estaticos.miarevista.es/uploads/images/recipe/58eba85e5bafe83d203c986b/ppal-chilaquiles_0.jpg',
+  },
+  {
+    title: 'Gnocchi',
+    description: 'With butter',
+    price: '$20.12',
+    image:
+      'https://www.kikkoman.eu/fileadmin/_processed_/a/e/csm_WEB_Spicy_tomato_and_sausage_gnocchi_with_fennel_and_spinach_b1e3a765d7.jpg',
+  },
+]
+
 export default function RestaurantDetails({ route, navigation }) {
   return (
     <View style={{ flex: 1 }}>
       <About route={route} />
       <Divider width={1.8} style={{ marginTop: 20 }} />
-      <MenuItems restaurantName={route.params.name} />
-      <ViewCart navigation={navigation} restaurantName={route.params.name} />
+      <MenuItems restaurantName={route.params.name} foods={foods} />
+      <ViewCart navigation={navigation} />
     </View>
   )
 }
